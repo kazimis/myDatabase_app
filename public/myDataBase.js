67faa18e41f9500fbc7a6d8ed6abd14ad275bc11
kazimis <sarajkazimi@gmail.com>
2019-10-07 16:17:57 -0700

    add

--- public/myDataBase.js
+++ public/myDataBase.js
@@ -1,4 +1,6 @@
-
+const query = client.query(
+  'CREATE TABLE Tokimon(id SERIAL PRIMARY KEY, text VARCHAR(40) not null, complete BOOLEAN)');
+query.on('end', () => { client.end(); });
 var myButton1 = document.getElementById("button1");
 var modal = document.getElementById("modal1");
 var exitSign = document.getElementById("exit1");
