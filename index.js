9c035ed1d4266cedfe2479724828714e2c024c8d
kazimis <sarajkazimi@gmail.com>
2019-10-14 19:41:11 -0700

    update

--- index.js
+++ index.js
@@ -6,20 +6,139 @@ const pool = new Pool({
 const express = require('express')
 const path = require('path')
 const PORT = process.env.PORT || 5000
+var bodyParser = require('body-parser');
+var app = express();
 
-express()
-  .use(express.static(path.join(__dirname, 'public')))
-  .set('views', path.join(__dirname, 'views'))
-  .set('view engine', 'ejs')
-  .get('/', (req, res) => res.render('pages/index'))
-  .get('/times', (req, res) => res.send(showTimes()))
-  .get('/db', async (req, res) => {
+  app.use(express.static(path.join(__dirname, 'public')));
+  app.use(express.json());
+  app.use(express.urlencoded({extended : true}));
+  app.set('views', path.join(__dirname, 'views'));
+  app.set('view engine', 'ejs');
+  app.get('/', (req, res) => res.render('pages/index'));
+
+  //get database home page
+  app.get('/myDataBase',(req,res)=> {
+    console.log("no");
+    var getUidNameQuerry = `select uid,name from tokimon`;
+      pool.query(getUidNameQuerry,(err,result)=>{
+        if(err)
+          res.end(err)
+        var r = { 'r': (result) ? result.rows : null};
+        res.render('pages/myDataBase', r );
+      //client.release();
+    })
+    });
+
+  //get addData page
+  app.get('/addData',(req,res)=>{res.render('pages/addData');
+  });
+
+  app.get('/update',(req,res)=>{res.render('pages/update');
+  });
+
+  app.get('/readAll',(req,res)=> {
+
+    var get_all_data = `SELECT * from tokimon`;
+      pool.query(get_all_data,(err,result)=>{
+        if(err)
+          res.end(err)
+        var results = { 'results': (result) ? result.rows : null};
+        res.render('pages/readAll', results );
+
+    })
+    });
+
+  app.get('/:id',(req,res) =>{
+    var id = req.params.id;
+
+    if(id.includes("del")){
+      var tmp = id.substr(3,id.length-1);
+
+      var deleteData_byID = `Delete from tokimon where uid = ${tmp}`;
+      pool.query(deleteData_byID, (err)=>{
+        if (err)
+          res.send(err);
+
+        });
+        var msg = "This data is deleted!";
+        res.render("pages/dataInfo", {'message':msg});
+
+    }else if(id.includes("update")){
+      var tmp = id.substr(6,id.length-1);
+      console.log(tmp);
+      var update_byID = `SELECT * FROM tokimon where uid = ${tmp}`;
+      pool.query(update_byID, (err,result)=>{
+
+        if (err)
+          res.send(err)
+        var results = { 'results': (result) ? result.rows : null};
+        console.log(results);
+        res.render('pages/update', results );
+      });
+
+    }else{
+      var getDataById = `SELECT * FROM tokimon where uid = ${id}`;
+
+        pool.query(getDataById, (err,result)=>{
+
+          if (err)
+            res.send(err)
+          var results = { 'results': (result) ? result.rows : null};
+          res.render('pages/dataInfo', results );
+
+        })
+    }
+    });
+  //after post HTML form
+  app.post("/:id",(req, res) => {
+    var myTotal = 0;
+    var id = req.params.id;
+    var myForm = req.body;
+    var myForm_length = Object.keys(myForm).length;
+    for(var i = 2; i < myForm_length-1; i++){
+      myTotal+= parseInt(myForm[i],10);
+    }
+    if(id.includes("update")){
+      var tmp = id.substr(6,id.length-1);
+
+      var updateQuerry = `update tokimon SET name = '${myForm[1]}',weight = '${myForm[2]}',
+      height = '${myForm[3]}',fly = '${myForm[4]}',fight = '${myForm[5]}',fire = '${myForm[6]}'
+      ,water = '${myForm[7]}',electric = '${myForm[8]}',frozen = '${myForm[9]}'
+      ,total =  ${myTotal} ,trainer_name = '${myForm[10]}' where uid = ${tmp}`;
+      pool.query(updateQuerry,(error)=>{
+        if (error){
+          res.send(error);
+        }
+        res.render('pages/dataInfo');
+      });
+
+    }
+    else if(id == "add"){
+      console.log(myTotal);
+      var userIn = `insert into tokimon (name,weight,height,fly,fight,fire,
+      water,electric,frozen,total,Trainer_Name)
+      Values('${myForm[1]}','${myForm[2]}','${myForm[3]}','${myForm[4]}',
+      '${myForm[5]}','${myForm[6]}','${myForm[7]}','${myForm[8]}',
+      '${myForm[9]}',${myTotal},'${myForm[10]}')`;
+      console.log(myForm[2]);
+      pool.query(userIn,(error)=>{
+        if (error){
+          res.send(error);
+        }
+        res.redirect('/myDataBase');
+      });
+    }
+
+    });
+
+
+  app.get('/times', (req, res) => res.send(showTimes()));
+
+  //for testing purpose
+  app.get('/db', async (req, res) => {
     try {
       const client = await pool.connect()
-      const query = client.query(
-        'CREATE TABLE Tokimon(id SERIAL PRIMARY KEY, text VARCHAR(40) not null, complete BOOLEAN)');
-      query.on('end', () => { client.end(); });
-      const result = await client.query('SELECT * FROM Tokimon');
+      const result = await client.query('SELECT Name FROM tokimon;');
       const results = { 'results': (result) ? result.rows : null};
       res.render('pages/db', results );
       client.release();
@@ -27,9 +146,11 @@ express()
       console.error(err);
       res.send("Error " + err);
     }
-  })
+  });
 
-  .listen(PORT, () => console.log(`Listening on ${ PORT }`))
+  app.listen(PORT, () => console.log(`Listening on ${ PORT }`));
+
+
   showTimes = () => {
   let result = ''
   const times = process.env.TIMES || 5
--- public/myDataBase.js
+++ public/myDataBase.js
@@ -1,13 +1,80 @@
-const query = client.query(
-  'CREATE TABLE Tokimon(id SERIAL PRIMARY KEY, text VARCHAR(40) not null, complete BOOLEAN)');
-query.on('end', () => { client.end(); });
-var myButton1 = document.getElementById("button1");
+
+var add_data_button = document.getElementById('addDataButton');
+var read_all_button = document.getElementById('readAllButton');
 var modal = document.getElementById("modal1");
 var exitSign = document.getElementById("exit1");
-myButton1.onmouseup = function(){
-  modal.style.display = "block";
+var mytable = document.getElementById("myTable");
+var del_link = document.getElementById("deleteLink");
+var update_link = document.getElementById("updateLink");
+var form = document.getElementById("updateForm");
+
+
+function gett(id){
+
+  location.href = "/".concat(id);
+
 }
 
-exitSign.onclick = function(){
-  modal.style.display = "none";
+update_link.onclick = function(){
+  var id = mytable.rows[1].cells[0].innerHTML;
+  location.href = "/update".concat(id);
+}
+
+del_link.onclick = function(){
+  var id = mytable.rows[1].cells[0].innerHTML;
+  location.href = "/del".concat(id);
+}
+add_data_button.onmouseup = function(){
+  window.location.pathname = '/addData';
+}
+read_all_button.onmouseup = function(){
+  window.location.pathname = '/readAll';
+}
+
+
+
+//function to calculate total,
+// function cal_total(){
+//   var num_of_elements = document.getElementById("myForm").length;
+//   var sum=0;
+//   for(var i = 1; i < num_of_elements-3; i++){
+//      var string_2_int = Number(document.getElementById("myForm")[i].value);
+//      sum += string_2_int;
+//     document.getElementById("myForm")[num_of_elements-3].value = sum;
+//   }
+// }
+
+
+//function to validate form entry before submission.
+function ifEmpty(){
+  var formLength = document.getElementById("myForm").length;
+
+  //check for entries if they are filled
+  for(var i = 0; i < formLength-1; i++){
+    var s = document.getElementById("myForm")[i].value;
+    //if at least one entry is filled and is valid.
+    if(s==""){
+        alert("There is at least one invalid entry! You can not submit this form!");
+        return false;
+    }
+  }
+
+  return true;
+}
+
+function ifEmpty_update(){
+  var formLength = document.getElementById("myForm").length;
+
+  //check for entries if they are filled
+  for(var i = 1; i < formLength-1; i++){
+    var s = document.getElementById("myForm")[i].value;
+    //if at least one entry is filled and is valid.
+    if(s!=""){
+        if(isNaN(s))
+          alert("There is at least one invalid entry! You can not submit this form!");
+        return false;
+    }
+  }
+  form.action = "/mydatabase".concat(location.href);
+  return true;
 }
